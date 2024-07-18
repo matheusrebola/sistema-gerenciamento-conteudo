b9c93c179d1e4494add6fa06354c306c4268c54e
matheusrebola <m.cavalcantir@outlook.com>
2024-07-17 23:24:06 -0300

    feat: criação do projeto spring

---
+++ sistema-gerenciamento-conteudo/src/main/java/sistemagerenciamentoconteudo/SistemaGerenciamentoConteudoApplication.java
@@ -0,0 +1,13 @@
+package sistemagerenciamentoconteudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoConteudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoConteudoApplication.class, args);
+	}
+
+}
---
+++ sistema-gerenciamento-conteudo/src/test/java/sistemagerenciamentoconteudo/SistemaGerenciamentoConteudoApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemagerenciamentoconteudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoConteudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
